1b41b893eec65e4c1240235537f2699e82711aaa
Keng Hong <kenghong95@gmail.com>
2023-03-24 15:31:31 +0800

    update meta details

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,62 @@
+<!-- <template>
+  <router-view />
+</template>
+
+<script setup>
+  //
+</script> -->
+
+
 <template>
   <router-view />
 </template>
 
-<!-- <script setup>
-  //
-</script> -->
+<script setup>
 
-<script>
-export default {
-  metaInfo: {
-    title: 'Wedding Invitation - Keng Hong & Joey',
-    meta: [
-      { property: 'og:title', content: 'Wedding Invitation - Keng Hong & Joey' },
-      { property: 'og:image', content: `${process.env.BASE_URL}/assets/img/cover-img-01.jpg` },
-      { property: 'og:description', content: 'Join us for a night of celebration at our wedding dinner! RSVP on this page to let us know you\'ll be there. We can\'t wait to share this special moment with you and honor our families and heritage!' },
-      { property: 'twitter:title', content: 'Wedding Invitation - Keng Hong & Joey' },
-      { property: 'twitter:image', content:  `${process.env.BASE_URL}/assets/img/cover-img-01.jpg` },
-      { property: 'twitter:description', content: 'Join us for a night of celebration at our wedding dinner! RSVP on this page to let us know you\'ll be there. We can\'t wait to share this special moment with you and honor our families and heritage!' },
-    ]
-  },
-}
 </script>
+
+
+
+<!-- import { defineComponent } from 'vue'
+import { useMeta } from 'vue-meta'
+
+export default defineComponent({
+  setup() {
+    const { meta } = useMeta()
+
+    // Set the meta info
+    meta.value = {
+      title: 'Wedding Invitation - Keng Hong & Joey',
+      meta: [
+        {
+          property: 'og:title',
+          content: 'Wedding Invitation - Keng Hong & Joey'
+        },
+        {
+          property: 'og:image',
+          content: `${process.env.BASE_URL}/assets/img/cover-img-01.jpg`
+        },
+        {
+          property: 'og:description',
+          content: 'Join us for a night of celebration at our wedding dinner! RSVP on this page to let us know you\'ll be there. We can\'t wait to share this special moment with you and honor our families and heritage!'
+        },
+        {
+          name: 'twitter:title',
+          content: 'Wedding Invitation - Keng Hong & Joey'
+        },
+        {
+          name: 'twitter:image',
+          content: `${process.env.BASE_URL}/assets/img/cover-img-01.jpg`
+        },
+        {
+          name: 'twitter:description',
+          content: 'Join us for a night of celebration at our wedding dinner! RSVP on this page to let us know you\'ll be there. We can\'t wait to share this special moment with you and honor our families and heritage!'
+        }
+      ]
+    }
+
+    return {
+      // any other data or functions
+    }
+  }
+}) -->
--- src/main.js
+++ src/main.js
@@ -1,12 +1,20 @@
-import { createApp } from 'vue'
-import { registerPlugins } from './plugins'
-import VueMeta from 'vue-meta'
+/**
+ * main.js
+ *
+ * Bootstraps Vuetify and other plugins then mounts the App`
+ */
+
+// Components
 import App from './App.vue'
+
+// Composables
+import { createApp } from 'vue'
+
+// Plugins
+import { registerPlugins } from '@/plugins'
 
 const app = createApp(App)
 
 registerPlugins(app)
-
-app.use(VueMeta.default)
 
 app.mount('#app')
