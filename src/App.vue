dbf68778c46f71525424687b6d924a1786ebacf0
Keng Hong <kenghong95@gmail.com>
2023-03-24 14:59:01 +0800

    update meta info

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,22 @@
   <router-view />
 </template>
 
-<script setup>
+<!-- <script setup>
   //
+</script> -->
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Wedding Invitation - Keng Hong & Joey',
+    meta: [
+      { property: 'og:title', content: 'Wedding Invitation - Keng Hong & Joey' },
+      { property: 'og:image', content: `${process.env.BASE_URL}/assets/img/cover-img-01.jpg` },
+      { property: 'og:description', content: 'Join us for a night of celebration at our wedding dinner! RSVP on this page to let us know you\'ll be there. We can\'t wait to share this special moment with you and honor our families and heritage!' },
+      { property: 'twitter:title', content: 'Wedding Invitation - Keng Hong & Joey' },
+      { property: 'twitter:image', content:  `${process.env.BASE_URL}/assets/img/cover-img-01.jpg` },
+      { property: 'twitter:description', content: 'Join us for a night of celebration at our wedding dinner! RSVP on this page to let us know you\'ll be there. We can\'t wait to share this special moment with you and honor our families and heritage!' },
+    ]
+  },
+}
 </script>
--- src/main.js
+++ src/main.js
@@ -1,20 +1,12 @@
-/**
- * main.js
- *
- * Bootstraps Vuetify and other plugins then mounts the App`
- */
-
-// Components
-import App from './App.vue'
-
-// Composables
 import { createApp } from 'vue'
-
-// Plugins
-import { registerPlugins } from '@/plugins'
+import { registerPlugins } from './plugins'
+import VueMeta from 'vue-meta'
+import App from './App.vue'
 
 const app = createApp(App)
 
 registerPlugins(app)
+
+app.use(VueMeta.default)
 
 app.mount('#app')
