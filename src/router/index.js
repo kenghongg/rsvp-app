14dcb89435098f8c3a1665a419ed4f9a09cb2f36
Keng Hong <kenghong95@gmail.com>
2023-10-05 19:36:52 +0800

    update simplified version

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,20 @@ const routes = [
     ],
   },
   {
+    path: '/s',
+    component: () => import('@/layouts/default/Default.vue'),
+    children: [
+      {
+        path: '/s',
+        name: 'Simplified Home',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "home" */ '@/views/SimplifiedHome.vue'),
+      },
+    ],
+  },
+  {
     path:'/registered',
     component: () => import('@/layouts/default/Default.vue'),
     children: [
---
+++ src/views/SimplifiedHome.vue
@@ -0,0 +1,45 @@
+<template>
+  <PlayBgMusic />
+  <HeaderStart />
+  <ImageSet />
+  <InviteIntro />
+  <!-- <ContentIntro /> -->
+  <!-- <ContentPreWed /> -->
+  <!-- <CoupleDetails /> -->
+  <CalendarShow />
+  <MapAnimation />
+
+  <!-- <RsvpForm /> -->
+  <!-- <BottomNavigation /> -->
+</template>
+
+<script>
+import BottomNavigation from "@/components/BottomNavigation.vue";
+import ContentPreWed from "@/components/ContentPreWed.vue";
+import InviteIntro from "@/components/InviteIntro.vue";
+import ContentIntro from "@/components/ContentIntro.vue";
+import ImageSet from "@/components/ImageSet.vue";
+import RsvpForm from "@/components/RsvpForm.vue";
+import PlayBgMusic from "@/components/PlayBgMusic.vue";
+import MapAnimation from "@/components/MapAnimation.vue";
+import CalendarShow from "@/components/CalendarShow.vue";
+import HeaderStart from "@/components/HeaderStart.vue";
+import CoupleDetails from "@/components/CoupleDetails.vue";
+
+
+export default {
+  components: {
+    BottomNavigation,
+    ContentPreWed,
+    InviteIntro,
+    // ContentIntro,
+    ImageSet,
+    RsvpForm,
+    PlayBgMusic,
+    MapAnimation,
+    CalendarShow,
+    HeaderStart,
+    CoupleDetails,
+  },
+};
+</script>
